3762de313bd2539696dbf9ba11ae9e9c7fccb065
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2021-01-05 14:16:09 +0330

    In the name of God

---
+++ README.md
@@ -0,0 +1,32 @@
+# Merge
+
+> **Merge javascript objects (`{}`) DEEPLY (recursively). It also checks the equality of input objects.**
+
+[![npm (scoped)](https://img.shields.io/npm/v/@graphix/merge.svg)](https://npmjs.com/package/@graphix/merge)
+[![install size](https://packagephobia.now.sh/badge?p=@graphix/merge)](https://packagephobia.now.sh/result?p=@graphix/merge)
+[![downloads](https://img.shields.io/npm/dt/@graphix/merge.svg)](https://npmjs.com/package/@graphix/merge) <br>
+[![dependencies](https://david-dm.org/Rahazad/merge.js.svg)](https://david-dm.org/Rahazad/merge.js)
+[![devDependencies](https://david-dm.org/Rahazad/merge.js/dev-status.svg)](https://david-dm.org/Rahazad/merge.js?type=dev) <br>
+[![license](https://img.shields.io/github/license/Rahazad/merge.js.svg)](https://github.com/Rahazad/merge.js/blob/master/LICENSE)
+[![Forks](https://img.shields.io/github/forks/Rahazad/merge.js.svg?style=social)](https://github.com/Rahazad/merge.js/fork)
+[![Stars](https://img.shields.io/github/stars/Rahazad/merge.js.svg?style=social)](https://github.com/Rahazad/merge.js)
+
+## Installation
+
+```bash
+npm i @graphix/merge
+```
+
+or using `yarn`:
+
+```bash
+yarn add @graphix/merge
+```
+
+## Usage
+
+Coming ...
+
+## License
+
+MIT © [Mir-Ismaili](https://github.com/mirismaili)
---
+++ src/index.js
@@ -0,0 +1,50 @@
+/**
+ * Created on 1399/9/19 (2020/12/9).
+ * @author {@link https://mirismaili.github.io S. Mahdi Mir-Ismaili}
+ */
+
+export const merge = (destObject, ...srcObjects) => {
+	if (!isObject(destObject))
+		throw new Error('`destObject` should be an Object!\n' + {destObject})
+
+	let equality = true
+
+	for (const [i, srcObj] of srcObjects.entries()) {
+		if (!isObject(srcObj))
+			throw new Error('Each of `srcObjects` should be an Object!\n' + {i, srcObj})
+
+		mergeR(destObject, srcObj)
+	}
+
+	return {destObject, equality}
+
+	function mergeR(dstObj, srcObj) {
+		for (const [k, subSrcObj] of Object.entries(srcObj)) {
+			if (isObject(subSrcObj)) {
+				if (!isObject(dstObj[k])) {
+					equality = false
+					dstObj[k] = {}
+				}
+			} else if (subSrcObj instanceof Array) {
+				if (!(dstObj[k] instanceof Array)) {
+					equality = false
+					dstObj[k] = []
+				}
+			} else {
+				if (!Object.is(dstObj[k], subSrcObj)) {
+					equality = false
+					dstObj[k] = subSrcObj
+				}
+				continue
+			}
+
+			mergeR(dstObj[k], subSrcObj)
+		}
+	}
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default merge
+
+//*****************************************************************************************/
+const isObject = variable => Object.prototype.toString.call(variable) === '[object Object]'
